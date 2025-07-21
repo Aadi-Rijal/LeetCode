38e8652cf233561a42357970f08d2b79ef956d51
Aadi Rijal <168012433+Aadi-Rijal@users.noreply.github.com>
2025-07-21 21:35:43 +0545

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 2:
+            return [0,1]
+        
+        for i in range(len(nums)):
+            req = target - nums[i]
+            if req in nums[i+1:]:
+                return [i , nums.index(req,i)]
