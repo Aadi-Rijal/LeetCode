1e16d714dec3e2712f28f0a5c43b699293802f95
Aadi Rijal <168012433+Aadi-Rijal@users.noreply.github.com>
2025-07-21 21:37:25 +0545

    Time: 286 ms (34.86%), Space: 18.6 MB (71.5%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -6,4 +6,4 @@ class Solution:
         for i in range(len(nums)):
             req = target - nums[i]
             if req in nums[i+1:]:
-                return [i , nums.index(req,i)]
+                return [i , nums.index(req,i+1)]
