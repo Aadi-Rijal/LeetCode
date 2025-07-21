381296ab81a9daf9678b471e3bda8914a40a6ca7
Aadi Rijal <168012433+Aadi-Rijal@users.noreply.github.com>
2025-07-21 22:42:35 +0545

    Time: 3 ms (80.12%), Space: 19.1 MB (9.67%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
@@ -2,6 +2,6 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         l = 0
         for r in range(len(nums)):
-            if num[r]:
-                num[l], num[r] = num[r], num[l]
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
