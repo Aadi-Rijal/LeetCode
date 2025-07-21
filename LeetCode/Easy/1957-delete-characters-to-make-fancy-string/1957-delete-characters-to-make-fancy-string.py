43f1aae18af65ef70cd8810a823fbc69a1bd096a
Aadi Rijal <168012433+Aadi-Rijal@users.noreply.github.com>
2025-07-21 21:05:27 +0545

    Time: 383 ms (34.59%), Space: 20.3 MB (10.54%) - LeetHub

--- LeetCode/Easy/1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ LeetCode/Easy/1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,11 +1,14 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        s_list= list(s)   
-        j= 0   
+        if len(s) < 3:
+            return s
 
-        for i in range(len(s_list)):
-            if j< 2 or s_list[j-2]!= s_list[i] or s_list[j-1]!= s_list[i]:
-                s_list[j]= s_list[i]
-                j+=1
+        s_list = list(s)
+        j = 2
 
-        return "".join(s_list[:j])       
+        for i in range(2, len(s)):
+            if s_list[i] != s_list[j - 1] or s_list[i] != s_list[j - 2]:
+                s_list[j] = s_list[i]
+                j += 1
+
+        return "".join(s_list[:j])
